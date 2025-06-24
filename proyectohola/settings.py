83b7a9070baae5bc538bff028768cbc9da28a1a9
pedropcao <pedro.catunta@cooperativailo.pe>
2025-06-24 15:42:53 -0500

    postgree

--- proyectohola/settings.py
+++ proyectohola/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+import dj_database_url
+from dotenv import load_dotenv
 #import django_heroku
 
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,12 +80,16 @@ WSGI_APPLICATION = 'proyectohola.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        #'NAME': BASE_DIR / 'db.sqlite3',
+#        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#    }
+#}
+#print(os.getenv('DATABASE_URL'))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
