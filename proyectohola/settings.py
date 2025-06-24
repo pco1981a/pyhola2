3f5633a1c50d67ec9b7fc26de62c65219174569d
pedropcao <pedro.catunta@cooperativailo.pe>
2025-06-24 13:03:45 -0500

    primer commit

--- proyectohola/settings.py
+++ proyectohola/settings.py
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+ALLOWED_HOSTS=['localhost','https://web-production-27a45.up.railway.app/'] # LA DIRECCION DE NUESTRA APP
